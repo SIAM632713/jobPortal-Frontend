824bc2d38d6075e3b31265ae68770bf8b9c50b96
siamxlr3 <siamxlr3@gmail.com>
2025-10-31 12:53:10 +0600

    Q8

--- src/utilitis/getBaseURL.js
+++ src/utilitis/getBaseURL.js
@@ -1,7 +1,3 @@
-// export const getBaseURL = () => {
-//     return "https://jobportal-backend-57hn.onrender.com"
-// }
-
 export const getBaseURL = () => {
-    return "http://localhost:7000"
+    return "https://jobportal-backend-57hn.onrender.com"
 }
